b1aac78c642746bf445e612b7abe7e001a2be967
tanawat <tanawat.jtl@gmail.com>
2021-12-23 18:04:32 +0700

    jab

--- route/Order.py
+++ route/Order.py
@@ -6,14 +6,25 @@ from datetime import datetime
 orders = Blueprint('order', __name__)
 
 @orders.route('/order')
-def order() :     
-    Dt =  run_query_fetchall(f"SELECT OrderId, MemberId, DocTime, Status FROM Orders where Status <> 0 and DocDate='{datetime.today().strftime('%Y-%m-%d')}' ") 
-    Dt1 =  run_query_fetchall(f"SELECT OrderId, MemberId, DocTime, Status FROM Orders where Status =  0 and DocDate='{datetime.today().strftime('%Y-%m-%d')}'  ") 
+def Index() :     
+    #sql = f"SELECT OrderId, MemberId, DocTime, Status FROM Orders WHERE Status <> 0 and DocDate = '{datetime.today().strftime('%Y-%m-%d')}'"    
+    sql = f"SELECT OrderId, Name, DocDate, DocTime, Status FROM Orders LEFT JOIN Member ON Orders.MemberId = Member.MemberId WHERE Status IN (1, 2)"  
+    dt_now =  run_query_fetchall(sql) 
+    dt1 =[]
+    for x in dt_now :
+        order = x[0]
+        menber = x[1]
+        date = str(library.FormatDate(x[2])) + ' ' + str(x[3])
+        status = x[4]
+        dt1.append([order, menber, date, status])
+    sql = f"SELECT OrderId, MemberId, DocTime, Status FROM Orders where Status =  0 and DocDate='{datetime.today().strftime('%Y-%m-%d')}'"
+    print(sql)
+    Dt1 =  run_query_fetchall(sql) 
     DateNow = library.FormatDate(datetime.now()) 
     TimeNow = library.FormatTime(datetime.now())
-    return render_template('order.html', data = Dt, data2 = Dt1, datenow = DateNow, timenow = TimeNow)
+    return render_template('Order/Index.html', data = dt1, data2 = Dt1, datenow = DateNow, timenow = TimeNow)
 
-@orders.route('/orderdetail/<id>')
+@orders.route('/order/orderdetail/<id>')
 def orderdetail(id):
     class order:
         def __init__(self, O_Id, O_MemberId, O_Date, O_Status, O_Phone, O_Address, O_Time, O_MemberName):
@@ -25,9 +36,9 @@ def orderdetail(id):
             self.O_Address = O_Address
             self.O_Time = O_Time
             self.O_MemberName = O_MemberName     
-    data1 =  run_query_fetchone(f"SELECT O_Id,O_MemberId,O_Date,O_Status,O_Phone,O_Address,O_Time,(select Name from member where MemberId=O_MemberId ) as O_MemberName  FROM orders where O_Id='{id}' ")     
+    data1 =  run_query_fetchone(f"SELECT OrderId, MemberId, DocDate, Status, Phone, Address, DocTime, MemberId as MemberName  FROM Orders where OrderId='{id}' ")     
     info = order(str(data1[0]),str(data1[1]),library.FormatDate(data1[2]) ,str(data1[3]),str(data1[4]),str(data1[5]),str(data1[6]),str(data1[7])) 
-    Dt =  run_query_fetchall(f"SELECT O_ProductId,(select Name from Product where O_ProductId=ID  ) as O_Productname, O_ProductAmount FROM ordersdetail where O_Id='{id}' ") 
-    return render_template('orderdetail.html',info=info,data=Dt)
+    Dt =  run_query_fetchall(f"SELECT OrderId, ProductId as ProductName, Amount FROM OrderSub where OrderId='{id}' ") 
+    return render_template('/Order/OrderDetail.html',info = info, data = Dt)
 
 
